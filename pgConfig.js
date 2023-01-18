d8f0bcc688f9666a1ecc764991afade3baab17ae
lexusegorov <alexey.egorov.job@gmail.com>
2023-01-18 14:37:38 +0700

    fix: fixed variable

--- pgConfig.js
+++ pgConfig.js
@@ -3,4 +3,4 @@ import pg from 'pg';
 
 dotenv.config();
 
-export const pool = new pg.Pool({connectionString: 'postgres://rhett89_bd_user:1kHVBsfTdLunzFoRm7LbTVcxaDh3zW2r@dpg-cf2mokirrk0bppc2800g-a.oregon-postgres.render.com/rhett89_bd'})
+export const pool = new pg.Pool({connectionString: process.env.DB_URL, ssl: {rejectUnauthorized: false}});
